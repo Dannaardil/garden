6ed3b0ffbc9c23cab6c70cebd6ef1b30aa90894a
Dannaardil <156458170+Dannaardil@users.noreply.github.com>
2024-05-20 20:52:28 -0500

    feat: :construction: se agregaron consultas a clients

--- js/module/clients.js
+++ js/module/clients.js
@@ -119,20 +119,149 @@ export const getClients = async()=>{
 }
 //3 multitabla
 
-export const getClientsNoPay= async()=>{
-    let res = await fetch("http://localhost:5501/clients")
+
+import { getSalesManager } from "./employees.js";
+import { getPayments } from "./payments.js";
+export const getClientsNoPay = async() => {
+    let res = await fetch("http://localhost:5501/clients");
     let dataClients = await res.json();
+    let dataSaleAgents = await getSalesManager();
+    let dataPayments = await getPayments();
+    let dataUpdated = [];
 
-    let clientsAndManager = await getClients();
+    dataClients.forEach(cliente => {
+        let hasPaid = false;
+
+        dataPayments.forEach(payment => {
+            if (cliente.client_code === payment.codigoClient) {
+                hasPaid = true;
+            }
+        });
+
+        if (!hasPaid) {
+            dataSaleAgents.forEach(agent => {
+                if (cliente.code_employee_sales_manager === agent.codigoEmpleado) {
+                    dataUpdated.push({
+                        nombreCliente: cliente.client_name,
+                        nombreRepresentante: agent.nombre,
+                        codigoOficina: agent.codigoOficina
+                    });
+                }
+            });
+        }
+    });
+
+    return dataUpdated;
+};
+
+// 2. 4. Devuelve el nombre de los clientes que han hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+import { getAllOfficesCodeAndCity } from "./offices.js";
+
+export const getClientsPay = async () => {
+        let clientManager = await getSalesManager();
+        let offices = await getAllOfficesCodeAndCity();
+        let dataUpdate = [];
+
+        clientManager.forEach(val => {
+            offices.forEach(ofi => {
+                if (val.codigoOficina == ofi.codigo) {
+                    dataUpdate.push({
+                        nombreCliente: val.nombreCliente,
+                        nombreRepresentante: val.nombreRepresentante,
+                        ciudadOficinaRepresentante: ofi.ciudad
+                    });
+                }
+            });
+        });
+
+        return dataUpdate;
+    } 
+
+// 2. 5. Devuelve el nombre de los clientes que **no** hayan hecho pagos y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+
+export const getClientNoPayAndSM= async () => {
+    let clientManager = await getClientNoPayAndSM();
+    let offices = await getAllOfficesCodeAndCity();
     let dataUpdate = [];
-    clientsAndManager.forEach(client => { 
-        
-        
+
+    clientManager.forEach(val => {
+        offices.forEach(ofi => {
+            if (val.codigoOficina == ofi.codigo) {
+                dataUpdate.push({
+                    nombreCliente: val.nombreCliente,
+                    nombreRepresentante: val.nombreRepresentante,
+                    ciudadOficinaRepresentante: ofi.ciudad
+                });
+            }
+        });
+    });
+
+    return dataUpdate;
+}
 
 
+// 2.7. Devuelve el nombre de los clientes y el nombre de sus representantes junto con la ciudad de la oficina a la que pertenece el representante.
+
+export const getAllClientsAndRepresentSalesOffices = async() => {
+    let client_manager = await getClientsName();
+    let office = await getAllOfficesCodeAndCity();
+    let dataUpdate=[];
+    client_manager.forEach(val=>{
+        office.forEach(dat=>{
+                dataUpdate.push({
+                    nombre_cliente: val.nombreCliente,
+                    nombre_representante:val.nombreRepresentante,
+                    ciudad_oficina_representante: dat.ciudad
+                })
+        })
     })
-
     
- return dataUpdate;
+    return dataUpdate
 
+
+}
+
+// 2.10. Devuelve el nombre de los clientes a los que no se les ha entregado a tiempo un pedido.
+
+export const getAllClients = async ()=>{
+    let res =await fetch("http://localhost:5501/clients")
+    let data=await res.json();
+    let dataUpdate = [];
+    let clientCodes = new Set();
+    data.forEach(val => {   
+        if (!clientCodes.has(val.client_code)) {
+            clientCodes.add(val.client_code);
+            dataUpdate.push({
+                codigoCliente: val.client_code,
+                nombreCliente: val.client_name,
+                nacionalidad: val.country,
+                ciudad: val.city
+            });
+        }
+    });
+    return dataUpdate;
+}
+
+
+
+
+
+import {getNotInTime} from "./requests.js"
+export const getAllClientsWithLateRequests = async() => {
+    
+    let dataLateRequests = await getNotInTime()
+    let dataClients = await getAllClients()
+    let dataUpdate = []
+
+    for (let lateRequest of dataLateRequests) {
+        for (let client of dataClients) {
+            if (client.codigoCliente == lateRequest.codigoCliente) {
+                dataUpdate.push({
+                    nombreCliente: client.nombreCliente
+                })
+            }
+        }
+    }
+
+        return dataUpdate
 }
